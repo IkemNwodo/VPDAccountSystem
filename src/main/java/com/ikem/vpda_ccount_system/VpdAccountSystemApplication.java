a901d1897499f66dc6191153675c07bf5ef319a2
Ikemefuna Nwodo <nwodofrank@gmail.com>
2023-04-14 22:44:50 +0100

    Update gitignore

--- src/main/java/com/ikem/vpda_ccount_system/VpdAccountSystemApplication.java
+++ src/main/java/com/ikem/vpda_ccount_system/VpdAccountSystemApplication.java
@@ -1,9 +1,7 @@
 package com.ikem.vpda_ccount_system;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VpdAccountSystemApplication {
